8adbdc3627d2ceac20523a88bcd2daeb22b23250
Heewon Lee <heewonlee@ewhain.net>
2022-07-17 18:16:11 +0900

    [add] fix for print() and requirements for server

--- test/main.py
+++ test/main.py
@@ -15,7 +15,7 @@ from sentence_transformers import SentenceTransformer
 from transformers import pipeline
 from googletrans import Translator
 from textrank import KeysentenceSummarizer
-# from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning.core.lightning import LightningModule
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
 
 # Create your views here.
@@ -72,163 +72,166 @@ def komoran_tokenizer(sent):
     return words
 
 
-class aiView(View):
-    def get_emotion(doc):
-        classifier = pipeline('zero-shot-classification',
-                              model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", device=0)
-        out = classifier(doc, ["love", "joy", "surprise", "anger",
-                               "sadness", "fear", "neutral", "tired"])
-        emotion = []
+def get_emotion(doc):
+    classifier = pipeline('zero-shot-classification',
+                          model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", device=0)
+    out = classifier(doc, ["love", "joy", "surprise", "anger",
+                           "sadness", "fear", "neutral", "tired"])
+    emotion = []
 
-        for i in range(8):
-            if out['scores'][i] > 0.4:
-                emotion.append(out['labels'][i])
+    for i in range(8):
+        if out['scores'][i] > 0.4:
+            emotion.append(out['labels'][i])
 
-        if len(emotion) == 0:
-            emotion.append(out['labels'][0])
-            emotion.append(out['labels'][1])
+    if len(emotion) == 0:
+        emotion.append(out['labels'][0])
+        emotion.append(out['labels'][1])
 
-        if(len(emotion) > 1):
-            # 창을 띄워서 사용자가 감정을 선택하는 코드로 바꾸기
-            print(emotion)
-            emotion_idx = int(
-                input('여러 개의 감정이 느껴지네요! 오늘을 대표하는 감정 1개를 선택해주세요(0, 1, 2, ...의 index로 입력):'))
-        else:
-            emotion_idx = 0
-
-        emo = emotion[emotion_idx]
-        # print(EMOTION)
-
-        return emo
-
-    def comment_emo(emotion):
-        # 감정에 따른 문구 출력
-        if (emotion == 'joy'):
-            emotion_comment = '오늘은 행복한 하루였군요!'
-        elif (emotion == 'love'):
-            emotion_comment = '당신의 하루에서 사랑이 느껴지네요.'
-        elif (emotion == 'surprise'):
-            emotion_comment = '오늘은 놀라운 일이 있었군요.'
-        elif (emotion == 'anger'):
-            emotion_comment = '오늘은 화가 많이 났던 하루였군요.'
-        elif (emotion == 'sadness'):
-            emotion_comment = '오늘은 조금 슬픈 하루였군요.'
-        elif (emotion == 'fear'):
-            emotion_comment = '오늘은 조금 무서웠던 일이 있었군요.'
-        elif (emotion == 'neutral'):
-            emotion_comment = '당신의 하루에서 평온함이 느껴지네요.'
-        else:
-            emotion_comment = '오늘은 조금 지치는 하루였군요.'
-
-        return emotion_comment
-
-    def keyword_extract(doc):
-        def mmr(doc_embedding, candidate_embeddings, words, top_n, diversity):
-            word_doc_similarity = cosine_similarity(
-                candidate_embeddings, doc_embedding)
-            word_similarity = cosine_similarity(candidate_embeddings)
-            keywords_idx = [np.argmax(word_doc_similarity)]
-            candidates_idx = [i for i in range(
-                len(words)) if i != keywords_idx[0]]
-
-            for _ in range(top_n - 1):
-                candidate_similarities = word_doc_similarity[candidates_idx, :]
-                target_similarities = np.max(
-                    word_similarity[candidates_idx][:, keywords_idx], axis=1)
-
-                mmr = (1-diversity) * candidate_similarities - \
-                    diversity * target_similarities.reshape(-1, 1)
-                mmr_idx = candidates_idx[np.argmax(mmr)]
-
-                keywords_idx.append(mmr_idx)
-                candidates_idx.remove(mmr_idx)
-            return [words[idx] for idx in keywords_idx]
-
-        okt = Okt()
-        tokenized_doc = okt.pos(doc)
-        tokenized_nouns = ' '.join([word[0]
-                                    for word in tokenized_doc if word[1] == 'Noun'])
-        n_gram_range = (1, 1)
-        count = CountVectorizer(
-            ngram_range=n_gram_range).fit([tokenized_nouns])
-        candidates = count.get_feature_names_out()
-        model = SentenceTransformer(
-            'sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
-        doc_embedding = model.encode([doc])
-        candidate_embeddings = model.encode(candidates)
-        top_n = 5
-        distances = cosine_similarity(doc_embedding, candidate_embeddings)
-        keywords = [candidates[index]
-                    for index in distances.argsort()[0][-top_n:]]
-
-        keyword = mmr(doc_embedding, candidate_embeddings,
-                      candidates, top_n=1, diversity=0.4)
-
-        translator = Translator()
-        key = translator.translate(keyword[0], dest='en').text
-
-        return key
-
-    def keySentence(doc):
-        komoran = Komoran()
-        summarizer = KeysentenceSummarizer(
-            tokenize=komoran_tokenizer,
-            min_sim=0.3,
-            verbose=False
-        )
-        sents_list = doc.split('.' or '?' or '!')
-        sents = []
-        for sent in sents_list:
-            if sent.strip() is not '':
-                sents.append(sent.strip())
-
-        bias = np.ones(len(sents))
-        bias[-1] = 5
-
-        keysents_list = summarizer.summarize(sents, topk=3, bias=bias)
-        keysents = []
-        for _, _, sent in keysents_list:
-            keysents.append(sent+'.')
-
-        return keysents
-
-    def comment_moon(keysents):
-        # KoGPT2에서 제공하는 토큰나이저 사용
-        model = KoGPT2Comment()
-        model.load_state_dict(torch.load("model_chp/comment_model.pth"))
-        model.eval()
-
-        moon_comment = ''
-        max_comment = ''
-        comment = ''
-        for sent in keysents:
-            comment = model.comment(sent)
-            if(comment[-1] is not '?' and (not '.' or not '!')):
-                comment = comment + '.'
-            if(len(max_comment) <= len(comment)):
-                max_comment = comment
-            if(len(comment) > 8 and '?' not in comment and '저' not in comment):
-                moon_comment = comment
-                break
-        if(moon_comment is ''):
-            moon_comment = max_comment
-
-        return moon_comment  # database에 moon_comment 저장하는 코드 추가하기
-
-    def ai(self, request):
-        doc = """
-            인공지능 중간고사를 친 다음날, 할게 없어서 북한산에 등산을 하러 갔다.
-            등산은 정말 오랜만이었는데, 가는 길은 험난했지만 백운대에 도착하니 너무 뿌듯하고 개운했다.
-            방안에 틀어박히기 보다 나와서 운동을 했더니 기분이 너무 좋았다.
-            앞으로 시간이 날 때마다 자주 산에 와야겠다.
-        """
-        emotion = get_emotion(doc)
-        comm_emo = comment_emo(emotion)
-        keyW = keyword_extract(doc)
-        keyS = keySentence(doc)
-        comm_moon = comment_moon(keyS)
-        comm = comm_emo + comm_moon
+    if(len(emotion) > 1):
+        # 창을 띄워서 사용자가 감정을 선택하는 코드로 바꾸기
         print(emotion)
-        print(comm)
-        print(keyW)
-        print(keyS)
+        emotion_idx = int(
+            input('여러 개의 감정이 느껴지네요! 오늘을 대표하는 감정 1개를 선택해주세요(0, 1, 2, ...의 index로 입력):'))
+    else:
+        emotion_idx = 0
+
+    emo = emotion[emotion_idx]
+    # print(EMOTION)
+
+    return emo
+
+
+def comment_emo(emotion):
+    # 감정에 따른 문구 출력
+    if (emotion == 'joy'):
+        emotion_comment = '오늘은 행복한 하루였군요!'
+    elif (emotion == 'love'):
+        emotion_comment = '당신의 하루에서 사랑이 느껴지네요.'
+    elif (emotion == 'surprise'):
+        emotion_comment = '오늘은 놀라운 일이 있었군요.'
+    elif (emotion == 'anger'):
+        emotion_comment = '오늘은 화가 많이 났던 하루였군요.'
+    elif (emotion == 'sadness'):
+        emotion_comment = '오늘은 조금 슬픈 하루였군요.'
+    elif (emotion == 'fear'):
+        emotion_comment = '오늘은 조금 무서웠던 일이 있었군요.'
+    elif (emotion == 'neutral'):
+        emotion_comment = '당신의 하루에서 평온함이 느껴지네요.'
+    else:
+        emotion_comment = '오늘은 조금 지치는 하루였군요.'
+
+    return emotion_comment
+
+
+def keyword_extract(doc):
+    def mmr(doc_embedding, candidate_embeddings, words, top_n, diversity):
+        word_doc_similarity = cosine_similarity(
+            candidate_embeddings, doc_embedding)
+        word_similarity = cosine_similarity(candidate_embeddings)
+        keywords_idx = [np.argmax(word_doc_similarity)]
+        candidates_idx = [i for i in range(
+            len(words)) if i != keywords_idx[0]]
+
+        for _ in range(top_n - 1):
+            candidate_similarities = word_doc_similarity[candidates_idx, :]
+            target_similarities = np.max(
+                word_similarity[candidates_idx][:, keywords_idx], axis=1)
+
+            mmr = (1-diversity) * candidate_similarities - \
+                diversity * target_similarities.reshape(-1, 1)
+            mmr_idx = candidates_idx[np.argmax(mmr)]
+
+            keywords_idx.append(mmr_idx)
+            candidates_idx.remove(mmr_idx)
+        return [words[idx] for idx in keywords_idx]
+
+    okt = Okt()
+    tokenized_doc = okt.pos(doc)
+    tokenized_nouns = ' '.join([word[0]
+                                for word in tokenized_doc if word[1] == 'Noun'])
+    n_gram_range = (1, 1)
+    count = CountVectorizer(
+        ngram_range=n_gram_range).fit([tokenized_nouns])
+    candidates = count.get_feature_names_out()
+    model = SentenceTransformer(
+        'sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+    doc_embedding = model.encode([doc])
+    candidate_embeddings = model.encode(candidates)
+    top_n = 5
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
+    keywords = [candidates[index]
+                for index in distances.argsort()[0][-top_n:]]
+
+    keyword = mmr(doc_embedding, candidate_embeddings,
+                  candidates, top_n=1, diversity=0.4)
+
+    translator = Translator()
+    key = translator.translate(keyword[0], dest='en').text
+
+    return key
+
+
+def keySentence(doc):
+    komoran = Komoran()
+    summarizer = KeysentenceSummarizer(
+        tokenize=komoran_tokenizer,
+        min_sim=0.3,
+        verbose=False
+    )
+    sents_list = doc.split('.' or '?' or '!')
+    sents = []
+    for sent in sents_list:
+        if sent.strip() != '':
+            sents.append(sent.strip())
+
+    bias = np.ones(len(sents))
+    bias[-1] = 5
+
+    keysents_list = summarizer.summarize(sents, topk=3, bias=bias)
+    keysents = []
+    for _, _, sent in keysents_list:
+        keysents.append(sent+'.')
+
+    return keysents
+
+
+def comment_moon(keysents):
+    # KoGPT2에서 제공하는 토큰나이저 사용
+    model = KoGPT2Comment()
+    model.load_state_dict(torch.load("model_chp/comment_model.pth"))
+    model.eval()
+
+    moon_comment = ''
+    max_comment = ''
+    comment = ''
+    for sent in keysents:
+        comment = model.comment(sent)
+        if(comment[-1] is not '?' and (not '.' or not '!')):
+            comment = comment + '.'
+        if(len(max_comment) <= len(comment)):
+            max_comment = comment
+        if(len(comment) > 8 and '?' not in comment and '저' not in comment):
+            moon_comment = comment
+            break
+    if(moon_comment is ''):
+        moon_comment = max_comment
+
+    return moon_comment  # database에 moon_comment 저장하는 코드 추가하기
+
+
+doc = """
+    인공지능 중간고사를 친 다음날, 할게 없어서 북한산에 등산을 하러 갔다.
+    등산은 정말 오랜만이었는데, 가는 길은 험난했지만 백운대에 도착하니 너무 뿌듯하고 개운했다.
+    방안에 틀어박히기 보다 나와서 운동을 했더니 기분이 너무 좋았다.
+    앞으로 시간이 날 때마다 자주 산에 와야겠다.
+"""
+emotion = get_emotion(doc)
+comm_emo = comment_emo(emotion)
+keyW = keyword_extract(doc)
+keyS = keySentence(doc)
+comm_moon = comment_moon(keyS)
+comm = comm_emo + comm_moon
+print(emotion)
+print(comm)
+print(keyW)
+print(keyS)
